2aa79afd9a89ff811a19865f3e2e14e4ca3352ed
Dominic Gitau <dominicnjoroge1@gmail.com>
2023-10-10 10:53:14 -0400

    function that queries the Reddit API and prints the titles of the first 10 hot posts listed for a given subreddit

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""
+Function to print first 10 titles from subreddit
+"""
+import requests
+
+
+def top_ten(subreddit):
+    """Retrieves using the Reddit API"""
+    link = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+    headers = {
+        'User-agent':
+        'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:105.0) \
+        Gecko/20100101 Firefox/105.0'
+    }
+
+    res = requests.get(url, headers=headers, allow_redirects=False)
+    data = res.json()
+    if res.status_code == 200:
+        for sub in data['data']['children']:
+            print(sub['data']['title'])
+    elif res.status_code == 404:
+        print(None)
